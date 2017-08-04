6f614d714d7a32459c114e50d102b93b403043e1
Paulius <pp@isgin.io>
2017-08-04 16:05:02 +0300

    PHPUnit TestCase namespace fix

--- TestCase.php
+++ TestCase.php
@@ -1,10 +1,9 @@
 <?php
 namespace Estina\Tests;
 
-use PHPUnit_Framework_TestCase;
 use ReflectionProperty;
 
-class TestCase extends PHPUnit_Framework_TestCase
+class TestCase extends \PHPUnit\Framework\TestCase
 {
     public function getContainerMock()
     {
